7425f2e3af3fc329dd261664be8d19b536dbe524
Sojo506 <fabiansojowork@gmail.com>
2023-05-09 16:50:49 -0600

    SelectPlan component mobile version styled

--- src/components/SelectPlan.jsx
+++ src/components/SelectPlan.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import styles from "../styles/selectPlan.module.css";
+
 const SelectPlan = () => {
+  const dataPlans = {
+    monthly: { arcade: 9, advanced: 12, pro: 15 },
+    yearly: { arcade: 90, advanced: 120, pro: 150 },
+  };
+
+  const [plan, setPlan] = useState(false);
+
+  const handleChange = (event) => {
+    setPlan(event.target.checked);
+  };
+
   return (
-    <div>SelectPlan</div>
-  )
-}
+    <section className="max-w-[320px] m-auto left-0 right-0 bg-n-white rounded-lg px-4 py-6 -translate-y-20">
+      <h2 className="text-p-marine-blue text-2xl font-bold mb-3">
+        Select your plan
+      </h2>
+      <p className="text-n-cool-gray mb-5">
+        You have the option of monthly or yearly billing.
+      </p>
+
+      <label className={`${styles.switch} rounded bg-n-magnolia`}>
+        <span
+          className={`${
+            !plan ? "text-p-marine-blue" : "text-n-cool-gray"
+          } font-medium`}
+        >
+          Monthly
+        </span>
+        <input
+          type="checkbox"
+          className={`${styles.checkbox}`}
+          onChange={handleChange}
+        />
+        <div className={`${styles.slider}`}></div>
+        <span
+          className={`${
+            plan ? "text-p-marine-blue" : "text-n-cool-gray"
+          } font-medium`}
+        >
+          Yearly
+        </span>
+      </label>
+    </section>
+  );
+};
 
 export default SelectPlan;
