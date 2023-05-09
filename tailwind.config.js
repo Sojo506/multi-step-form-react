aa8de954e5964e6c001affab772957caaad9ec44
Sojo506 <fabiansojowork@gmail.com>
2023-05-09 11:32:30 -0600

    colores added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
     fontFamily: {
       ubuntu: ["Ubuntu", "sans-serif"],
     },
+    colors: {
+      /* PRIMARY COLORS */
+      "p-marine-blue": "hsl(213, 96%, 18%)",
+      "p-purplish-blue": "hsl(243, 100%, 62%)",
+      "p-pastel-blue": "hsl(228, 100%, 84%)",
+      "p-light-blue": "hsl(206, 94%, 87%)",
+      "p-strawberry-red": "hsl(354, 84%, 57%)",
+
+      /* NEUTRAL COLORS */
+      "n-cool-gray": "hsl(231, 11%, 63%)",
+      "n-light-gray": "hsl(229, 24%, 87%)",
+      "n-magnolia": "hsl(217, 100%, 97%)",
+      "n-alabaster": "hsl(231, 100%, 99%)",
+      "n-white": "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
 };
