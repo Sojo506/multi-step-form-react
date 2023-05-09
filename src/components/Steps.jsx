1192cf2c955b0d523765723c4106cc90d4dac336
Sojo506 <fabiansojowork@gmail.com>
2023-05-09 11:44:49 -0600

    Steps component ready by the moment

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "../styles/steps.module.css"
 
 const Steps = ({ activeStep }) => {
   return (
-    <div className="bg-slate-200">
-      <div>
+    <div className={`${styles.stepsContainer}`}>
+      <div className="absolute top-5 w-full flex justify-center gap-3">
         <Button step={1} text={"your info"} activeStep={activeStep} />
         <Button step={2} text={"select plan"} activeStep={activeStep} />
         <Button step={3} text={"add-ons"} activeStep={activeStep} />
